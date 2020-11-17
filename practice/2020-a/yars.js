835f42c767d62c23f128ed5faec69e9b37ab2170
Ruud Helderman <HELD1353@exactsoftware.com>
2020-11-17 22:57:42 +0100

    Practice 3

--- practice/2020-a/yars.js
+++ practice/2020-a/yars.js
@@ -9,15 +9,22 @@ ctx.textAlign = 'right';
 var framecount = 0;
 var zeroes = '00000000';
 var score = 0, scount = 0;
+var mx = 320, my = 500;
 
 function main() {
+	window.ship = document.getElementById('ship');
 	canvas.onclick = function() {
 		score += 25;
+	};
+	canvas.onmousemove = function(e) {
+		mx = e.pageX - canvas.offsetLeft;
+		my = e.pageY - canvas.offsetTop;
 	};
 	(function loop() {
 		requestAnimationFrame(loop);
 		scount += scount < score;
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.drawImage(ship, mx, my);
 		ctx.fillText(zeroes + scount, ctx.measureText(zeroes).width, 40);
 		++framecount;
 	})();
