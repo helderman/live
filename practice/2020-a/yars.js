d2f103fb74b40737b9c1cec1189bad7f1a0bdcb8
Ruud Helderman <HELD1353@exactsoftware.com>
2020-11-17 23:15:06 +0100

    Practice 6

--- practice/2020-a/yars.js
+++ practice/2020-a/yars.js
@@ -19,14 +19,28 @@ function family(a) {
 
 
 var sprite = {
+	collide: function() {
+		this.hit = !this.dead && this.img.collide(this.x, this.y);
+	},
 	draw: function() {
-		this.img.draw(this.x, this.y);
-	}
+		!this.dead && this.img.draw(this.x, this.y);
+	},
+	explode: function() {
+		if (this.hit) {
+			this.dead = framecount + this.xsize;
+			score += this.xscore;
+		}
+	},
+	dead: 0,
+	xsize: 15,
+	xscore: 0
 };
 
 function Ship() {
+	this.xsize = 30;
 	this.img = document.getElementById('ship');
 	this.move = function() {
+		if (this.dead) return;
 		this.x = mx;
 		this.y = my;
 	};
@@ -36,19 +50,42 @@ Ship.prototype = sprite;
 var fired = 0;
 
 function Bullet() {
+	this.xsize = 7;
 	this.y = -999;
 	this.img = document.getElementById('bullet');
 	this.move = function() {
-		if (this.y > -99) {
+		if (!this.dead && this.y > -99) {
 			this.y -= 20;
 		}
-		else if (!--fired) {
+		else if (framecount > this.dead && !--fired) {
+			this.dead = 0;
 			this.x = ship.x;
 			this.y = ship.y - 50;
 		}
 	};
 }
 Bullet.prototype = sprite;
+
+var lag = 0;
+
+function Invader(num) {
+	this.lag = lag += 25;
+	//this.dead = -1;
+	this.xscore = 5;
+	this.img = document.getElementById('inv' + num);
+	this.move = function() {
+		if (t < -1.9 || t > 1.9) this.dead = 0;
+		var t = 2 * Math.sin((framecount - this.lag) / 300);
+		if (this.dead) return;
+		this.moveTo(t, 0);
+	};
+	this.moveTo = function(x, y) {
+		this.x = canvas.width * (0.5 - x);
+		this.y = canvas.height * (0.5 - y);
+	};
+}
+Invader.prototype = sprite;
+
 
 
 
@@ -58,8 +95,13 @@ function main() {
 		ship,
 		new Bullet(), new Bullet(), new Bullet(), new Bullet(),
 	]);
+	var bad = family([
+		new Invader(1), new Invader(1), new Invader(1), new Invader(1),
+		new Invader(2), new Invader(2), new Invader(2), new Invader(2),
+		new Invader(3), new Invader(3), new Invader(3), new Invader(3),
+	]);
 	canvas.onclick = function() {
-		fired = 1;
+		fired = !ship.dead;
 	};
 	canvas.onmousemove = function(e) {
 		mx = e.pageX - canvas.offsetLeft;
@@ -69,8 +111,18 @@ function main() {
 		requestAnimationFrame(loop);
 		scount += scount < score;
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		bad('move');
 		good('move');
+		bad('draw');
+		good('collide');
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		good('draw');
+		bad('collide');
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		bad('draw');
+		good('draw');
+		bad('explode');
+		good('explode');
 		ctx.fillText(zeroes + scount, ctx.measureText(zeroes).width, 40);
 		++framecount;
 	})();
@@ -82,6 +134,16 @@ for (var i = imgs.length; i--;) {
 	imgs[i].onload = function() {
 		var w = this.width;
 		var h = this.height;
+		ctx.drawImage(this, 0, 0);
+		var shape = ctx.getImageData(0, 0, w, h).data;
+		ctx.clearRect(0, 0, w, h);
+		this.collide = function(x, y) {
+			var a = ctx.getImageData(x - w/2 | 0, y - h/2 | 0, w, h).data;
+			for (var i = 3; i < a.length; i += 4) {
+				if (a[i] + shape[i] > 500) return true;
+			}
+			return false;
+		};
 		this.draw = function(x, y) {
 			ctx.drawImage(this, x - w/2 | 0, y - h/2 | 0);
 		};
