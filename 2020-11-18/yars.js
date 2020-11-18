a4e93332293b033494425609969bc70cdaff41f6
Ruud Helderman <HELD1353@exactsoftware.com>
2020-11-18 11:13:32 +0100

    TechTalks commit 3

--- 2020-11-18/yars.js
+++ 2020-11-18/yars.js
@@ -22,10 +22,24 @@ function main() {
 	(function loop() {
 		requestAnimationFrame(loop);
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.drawImage(ship, mx, my);
+		ship.draw(mx, my);
 		scount += scount < score;
 		ctx.fillText(zeroes + scount, ctx.measureText(zeroes).width, 40);
 		++framecount;
 	})();
 }
-main();
+var toload = 1;
+var imgs = document.getElementsByTagName('img');
+for (var i = imgs.length; i--;) {
+	++toload;
+	imgs[i].onload = function() {
+		var w = this.width;
+		var h = this.height;
+		this.draw = function(x, y) {
+			ctx.drawImage(this, x - w/2 | 0, y - h/2 | 0);
+		};
+		--toload || main();
+	};
+	imgs[i].src = imgs[i].id + '.png';
+}
+--toload || main();
