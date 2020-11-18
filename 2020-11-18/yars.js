e76702f783faffec5c12530d5c428759d18c3d91
Ruud Helderman <HELD1353@exactsoftware.com>
2020-11-18 11:10:28 +0100

    TechTalks commit 2

--- 2020-11-18/yars.js
+++ 2020-11-18/yars.js
@@ -8,14 +8,21 @@ ctx.textAlign = 'right';
 var framecount = 0;
 var zeroes = '00000000';
 var score = 0, scount = 0;
+var mx = 320, my = 500;
 
 function main() {
+	window.ship = document.getElementById('ship');
 	canvas.onclick = function() {
 		score += 25;
+	};
+	canvas.onmousemove = function(e) {
+		mx = e.pageX - canvas.offsetLeft;
+		my = e.pageY - canvas.offsetTop;
 	};
 	(function loop() {
 		requestAnimationFrame(loop);
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.drawImage(ship, mx, my);
 		scount += scount < score;
 		ctx.fillText(zeroes + scount, ctx.measureText(zeroes).width, 40);
 		++framecount;
