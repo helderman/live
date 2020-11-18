0a8cdeade0c63db380437c63550ab856e159984b
Ruud Helderman <HELD1353@exactsoftware.com>
2020-11-18 11:21:57 +0100

    TechTalks commit 5

--- 2020-11-18/yars.js
+++ 2020-11-18/yars.js
@@ -48,11 +48,32 @@ function Bullet() {
 }
 Bullet.prototype = sprite;
 
+var lag = 0;
+
+function Invader(num) {
+	this.lag = lag += 25;
+	this.img = document.getElementById('inv' + num);
+	this.move = function() {
+		var t = 2 * Math.sin((framecount - this.lag) / 300);
+		this.moveTo(t, 0);
+	};
+	this.moveTo = function(x, y) {
+		this.x = canvas.width * (0.5 - x);
+		this.y = canvas.height * (0.5 - y);
+	};
+}
+Invader.prototype = sprite;
+
 function main() {
 	window.ship = new Ship();
 	var good = family([
 		ship,
 		new Bullet(), new Bullet(), new Bullet(), new Bullet(),
+	]);
+	var bad = family([
+		new Invader(1), new Invader(1), new Invader(1), new Invader(1),
+		new Invader(2), new Invader(2), new Invader(2), new Invader(2),
+		new Invader(3), new Invader(3), new Invader(3), new Invader(3),
 	]);
 	canvas.onclick = function() {
 		fired = 1;
@@ -64,7 +85,9 @@ function main() {
 	(function loop() {
 		requestAnimationFrame(loop);
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		bad('move');
 		good('move');
+		bad('draw');
 		good('draw');
 		scount += scount < score;
 		ctx.fillText(zeroes + scount, ctx.measureText(zeroes).width, 40);
